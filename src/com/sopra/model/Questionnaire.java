c3e32656a39e79888e742553cafa3b02730ad3b5
Manon <lmn.manon@gmail.com>
2017-09-13 11:22:26 +0200

    Modifs sur Questionnaire

--- src/com/sopra/model/Questionnaire.java
+++ src/com/sopra/model/Questionnaire.java
@@ -1,11 +1,13 @@
 package com.sopra.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 public class Questionnaire implements Serializable {
 	//pour enlever les erreurs jaunes qu'on peut enlever
@@ -21,5 +23,50 @@ public class Questionnaire implements Serializable {
 	
 	@OneToMany(mappedBy="questionnaire")
 	private List<Question> questions;
+
+	//getters and setters
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the questions
+	 */
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	/**
+	 * @param questions the questions to set
+	 */
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+	
+	
 	
 }
