82a7240db3854355c4d953997283ff4210715f06
Manon <lmn.manon@gmail.com>
2017-09-13 10:59:26 +0200

    Creation class Questionnaire et Test

---
+++ src/com/sopra/model/Questionnaire.java
@@ -0,0 +1,11 @@
+package com.sopra.model;
+
+import java.io.Serializable;
+
+public class Questionnaire implements Serializable {
+	//pour enlever les erreurs jaunes qu'on peut enlever
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
