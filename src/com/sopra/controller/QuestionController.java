50d9fbe8beef4ef6b517d0215c9647d4b60921c6
Unknown <charles.brebion@gmail.com>
2017-09-13 14:41:16 +0200

    QuestionController sans croix rouge

--- src/com/sopra/controller/QuestionController.java
+++ src/com/sopra/controller/QuestionController.java
@@ -1,16 +1,68 @@
 package com.sopra.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.sopra.dao.IQuestionDAO;
+import com.sopra.dao.IQuestionnaireDAO;
+import com.sopra.model.Question;
+import com.sopra.model.Questionnaire;
 
 @Controller
 public class QuestionController {
+	private static final String ATT_QUESTION			= "question";
+	private static final String ATT_QUESTIONS			= "questions";
 	
 	@Autowired
 	private IQuestionDAO questionHibernateDAO;
 	
-	@RequestMapping(value="")
+	@Autowired
+	private IQuestionnaireDAO questionnaireHibernateDAO;
+	
+	@RequestMapping(value="/{idQuestionnaire}/questions", method = RequestMethod.GET)
+	public String question(@PathVariable(value="idQuestionnaire", required=true) int idQuestionnaire,
+							Model model) {
+		
+		Questionnaire quest = questionnaireHibernateDAO.find(idQuestionnaire);
+		List<Question> questions = quest.getQuestions();
+		
+		model.addAttribute(ATT_QUESTIONS, questions);
+		
+		return idQuestionnaire + "/questions";
+	}
+	
+	
+	@RequestMapping(value="/{idQuestionnaire}/modifier/{idQuestion}", method = RequestMethod.GET)
+	public String modifierQuestion(@PathVariable(value="idQuestionnaire", required=true) int idQuestionnaire,
+									@PathVariable(value="idQuestion", required=true) int idQuestion,
+									Model model) {
+		
+		Question question = questionHibernateDAO.find(idQuestion);
+		model.addAttribute(ATT_QUESTION, question);
+		
+		return idQuestionnaire + "/modifierQuestion/" + idQuestion;
+	}
+	
+	
+	@RequestMapping(value="/{idQuestionnaire}/modifier/{idQuestion}", method = RequestMethod.POST)
+	public String modifierQuestion(@PathVariable(value="idQuestionnaire", required=true) int idQuestionnaire,
+									@PathVariable(value="idQuestion", required=true) int idQuestion,
+									@Valid @ModelAttribute("question") Question q,
+									BindingResult result,
+									Model model) {
+		
+		return "redirect:/" + idQuestionnaire + "/questions";
+	}
+	
 
 }
