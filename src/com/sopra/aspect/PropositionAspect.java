769855c5618e794ceee9389606029e75fdd6edbd
Manon <lmn.manon@gmail.com>
2017-09-14 15:33:44 +0200

    Finalisation modification proposition avec Spring AOP

--- src/com/sopra/aspect/PropositionAspect.java
+++ src/com/sopra/aspect/PropositionAspect.java
@@ -1,5 +1,7 @@
 package com.sopra.aspect;
 
+import java.util.List;
+
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 
 import com.sopra.dao.IPropositionDAO;
 import com.sopra.model.Proposition;
+import com.sopra.model.Question;
 
 @Controller
 @Aspect
@@ -14,8 +17,22 @@ public class PropositionAspect {
 	@Autowired 
 	private IPropositionDAO propositionDAO;
 	
-	@Before("execution(public com.sopra.dao.hibernate.PropositionDAO(..)) && args(proposition)")
+	@Before("execution(* com.sopra.dao.hibernate.PropositionHibernateDAO.save(..)) && args(proposition)")
 	public void modificationBonneReponse(Proposition proposition) {
+		//on récupère la question associée à la proposition qui va être sauvegardée dans la bdd
+		Question question = proposition.getQuestion();
+		//on récupère la liste des propositions associées à la question
+		List<Proposition> propositions = question.getPropositions();
+		
+		if(proposition.isCorrect()) {
+			for(Proposition prop : propositions) {
+				if(prop.isCorrect()) {
+					prop.setCorrect(false);
+					prop = propositionDAO.save(prop);
+				}
+			}
+		}
+		
 		
 	}
 
